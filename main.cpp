22cc0b9f8222649a7f17f168b89685b2ce3e396b
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-31 22:33:15 +0600

    lab05classcode

--- main.cpp
+++ main.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
-#include "UnsortedType.h"
-#include "UnsortedType.cpp"
+#include "SortedType.h"
+#include "SortedType.cpp"
 
 using namespace std;
 
 int main()
 {
-    UnsortedType<int>t1;
-    int n=0;
+    SortedType<int>t;
+    cout<<t.LengthIs();
     int k;
-    while(n<=5){
+    for(int i=0;i<MAX_ITEMS;i++){
         cin>>k;
-        t1.InsertItem(k);
-        n++;
+        t.InsertItem(k);
 
 
     }
-    cout<<t1.LengthIs()<<endl;
+    t.print();
+    cout<<endl;
+    int m;
+    cout<<"enter a value to retrieve "<<endl;
+    cin>>m;
+    bool q=true;
+    t.RetrieveItem(m,q);
+    if(t.IsFull()==true)
+    cout<<"Full"<<endl;
+    else
+        cout<<"notFull"<<endl;
+        int o;
+        cout<<"enter a value to delete"<<endl;
+        cin>>o;
+
+
+        t.DeleteItem(o);
+
+        cout<<"after deleting item"<<endl;
+        t.print();
+        cout<<"after deleting checking whether the list is full or not"<<endl;
+        if(t.IsFull()==true){
+            cout<<"Full"<<endl;
+
+        }
+        else
+            cout<<"Not full"<<endl;
+    return 0;
 }
