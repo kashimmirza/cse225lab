f3b842af89b7d281f30f0870c6147aeacd04ec9e
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-11-02 12:40:36 +0545

    stacktype.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,80 @@
 #include <iostream>
-#include "SortedType.h"
-#include "SortedType.cpp"
+#include "stacktype.h"
+#include "stackType.cpp"
 
 using namespace std;
 
 int main()
-{
-    SortedType<int>t;
-    cout<<t.LengthIs();
-    int k;
-    for(int i=0;i<MAX_ITEMS;i++){
-        cin>>k;
-        t.InsertItem(k);
 
+{
+    StackType<int>t;
+    if(t.IsEmpty()==true){
+        cout<<"Empty"<<endl;
 
     }
-    t.print();
-    cout<<endl;
-    int m;
-    cout<<"enter a value to retrieve "<<endl;
-    cin>>m;
-    bool q=true;
-    t.RetrieveItem(m,q);
-    if(t.IsFull()==true)
-    cout<<"Full"<<endl;
+    else{
+        cout<<"Full"<<endl;
+
+    }
+    int k;
+
+    for(int i=0;i<MAX_ITEMS-1;i++){
+            cin>>k;
+        t.Push(k);
+
+    }
+    if(t.IsEmpty()==true){
+        cout<<"empty"<<endl;
+
+    }
     else
-        cout<<"notFull"<<endl;
-        int o;
-        cout<<"enter a value to delete"<<endl;
-        cin>>o;
+        cout<<"not empty"<<endl;
+    if(t.IsFull()==true){
+        cout<<"Full"<<endl;
+
+    }
+    else{
+
+        cout<<"NOtfull"<<endl;
+    }
+    StackType<int>t2;
+    cout<<t.Top()<<endl;
+    for(int i=0;i<4;i++){
+    t2.Push(t.Top());
+    t.Pop();
+}
+int i=5;
+while(i--){
+
+t.Push(t2.Top());
+cout<<t.Top()<<" ";
+
+ t2.Pop();
+i++;
+if(i==4){
+    t.Push(7);
+}
+}
+
+/*for(int i=0;i<4;i++){
+    t3.Push(t2.Top());
+    cout<<t3.Top()<<" ";
+
+    //cout<<t2.Top()<<" ";
+    //t2.Pop();
+}*/
+
+t.Push(7);
+if(t.IsFull()==true){
+    cout<<"stackisfull"<<endl;
+}
+
+/*for(int i=0;i<4;i++){
+    cout<<t3.Top();
+     t3.Pop();
+
+}*/
 
 
-        t.DeleteItem(o);
-
-        cout<<"after deleting item"<<endl;
-        t.print();
-        cout<<"after deleting checking whether the list is full or not"<<endl;
-        if(t.IsFull()==true){
-            cout<<"Full"<<endl;
-
-        }
-        else
-            cout<<"Not full"<<endl;
     return 0;
 }
